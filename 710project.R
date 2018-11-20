04974f98f999f2e1f2494b45f1b754b39b0def86
Xin Gu <innershine.xg@gmail.com>
2018-11-19 22:04:51 -0500

    Add files via upload

--- 710project.R
+++ 710project.R
@@ -1,5 +1,5 @@
 library(readr)
-output_run <- read_csv("C:/Users/inner/Downloads/output_run.csv")
+output_run <- read_csv("C:/Users/inner/Downloads/Airbnb/output_run.csv")
 ratingclass<-output_run
 range(output_run$review_scores_rating)
 table(output_run$review_scores_rating)
@@ -7,20 +7,21 @@ table(output_run$review_scores_rating)
 hist(output_run$review_scores_rating)
 class(ratingclass$review_scores_rating)
 ratingclass$review_scores_rating <-ifelse(ratingclass$review_scores_rating>=95,'High','Low')
-getwd()
-write.csv(ratingclass,'C:/Users/inner/Downloads/ratingclass.csv')
+table(ratingclass$review_scores_rating)
+# remove superhost
+# ratingclass1=ratingclass[,-3]
+ratingclass$review_scores_rating=factor(ratingclass$review_scores_rating)
+ratingclass$zipcode=factor(ratingclass$zipcode)
 #data for salford
-
-#remove superhost
-ratingclass1=ratingclass[,-3]
-ratingclass1$review_scores_rating=factor(ratingclass1$review_scores_rating)
+getwd()
+write.csv(ratingclass,'C:/Users/inner/Downloads/Airbnb/ratingclass.csv')
 
 
 #split into train/test sets
 set.seed(1)###For reproducible results
-trainset=sample(1:nrow(ratingclass1), 0.8*nrow(ratingclass1))
-train=ratingclass1[trainset,]
-testdata=ratingclass1[-trainset,]
+trainset=sample(1:nrow(ratingclass), 0.8*nrow(ratingclass))
+train=ratingclass[trainset,]
+testdata=ratingclass[-trainset,]
 
 
 #CART TREE
@@ -43,22 +44,19 @@ summary(c50fit)
 #test models
 #CART TREE
 pred.cart<- predict(cart, newdata = testdata,type="class")
-### factor neighbourhood_cleansed has new levels Lincoln Park # only 1 property
-testdata1=testdata[testdata$neighbourhood_cleansed!= "Lincoln Park",]
-pred.cart<- predict(cart, newdata = testdata1,type="class")
-table(pred.cart,testdata1$review_scores_rating)
-#pred.cart High  Low
-#     High 1196  486
-#     Low    93  131
-(1196+131)/1907 #0.6958574
+table(pred.cart,testdata$review_scores_rating)
+# pred.cart High  Low
+# High 1189  479
+# Low   101  138
+(1189+138)/1907 #0.6958574
 
 #C5.0 TREE
-pred.c50 <- predict(c50fit, newdata = testdata1,type="class")
-table(pred.c50 ,testdata1$review_scores_rating)
-#pred.c50 High  Low
-#   High  1203  451
-#   Low     86  166
-(1203+166)/1907 #0.7178815
+pred.c50 <- predict(c50fit, newdata = testdata,type="class")
+table(pred.c50 ,testdata$review_scores_rating)
+# pred.c50 High  Low
+# High 1200  470
+# Low    90  147
+(1200+147)/1907 #0.706345
 
 #over sampling
 library(ROSE)
@@ -100,38 +98,39 @@ summary(c50fit1)
 #CART TREE
 pred.cart1<- predict(cart1, newdata = testdata,type="class")
 #factor host_response_time has new levels None # 3 properties
-testdata2=testdata[testdata$host_response_time!='None',] 
-
-pred.cart1<- predict(cart1, newdata = testdata2,type="class")
-#factor neighbourhood_cleansed has new levels Lincoln Park
-testdata2=testdata2[testdata2$neighbourhood_cleansed!= "Lincoln Park",]#1 property
-
-pred.cart1<- predict(cart1, newdata = testdata2,type="class")
-table(pred.cart1,testdata2$review_scores_rating)
-#pred.cart1 High Low
-#     High  836 238
-#     Low   450 379
-(836+379)/1903 #0.6384656
+testdata1=testdata[testdata$host_response_time!='None',] 
+pred.cart1<- predict(cart1, newdata = testdata1,type="class")
+table(pred.cart1,testdata1$review_scores_rating)
+# pred.cart1 High Low
+# High  980 298
+# Low   307 319
+(980+319)/1904 #0.6822479
 
 
 #C5.0 TREE
-pred.c501 <- predict(c50fit1, newdata = testdata2,type="class")
-table(pred.c501 ,testdata2$review_scores_rating)
-#pred.c501 High Low
-#     High  931 285
-#     Low   355 332
-(931+332)/1903
+pred.c501 <- predict(c50fit1, newdata = testdata1,type="class")
+table(pred.c501 ,testdata1$review_scores_rating)
+# pred.c501 High Low
+# High  927 279
+# Low   360 338
+(927+338)/1904 #0.6643908
 
 
-#random forest###problem with bootstrap sampling
+#random forest#
 library(randomForest)
-sum(is.na(train))
-#https://www.rdocumentation.org/packages/stats/versions/3.5.1/topics/na.fail
-train=na.omit(train)
-sum(is.na(train))
-RF=randomForest(review_scores_rating ~.,data =train)
+# sum(is.na(train))
+# #https://www.rdocumentation.org/packages/stats/versions/3.5.1/topics/na.fail
+# train=na.omit(train)
+# sum(is.na(train))
+library(dplyr)
+ratingclass1=ratingclass %>% mutate_if(is.character, as.factor)
+RF=randomForest(review_scores_rating ~ .,data =ratingclass1,
+                importance=TRUE, 
+                proximity=TRUE,
+                na.action=na.omit)
 RF
-
+importance(RF)        
+varImpPlot(RF)
 
 # Bagging model (5 base models)
 set.seed(2)
@@ -147,60 +146,50 @@ cart1 <- rpart(review_scores_rating ~.,data =s1,
                minsplit=30, cp=0.01,
                method = "class")
 p1 <- predict(cart1, newdata = testdata)
-testdata3=testdata[testdata$host_response_time!='None',]
-p1 <- predict(cart1, newdata = testdata3)
-testdata3=testdata3[testdata3$neighbourhood_cleansed!='Lincoln Park'& 
-                     testdata3$neighbourhood_cleansed!= 'Terra Nova',]
-p1 <- predict(cart1, newdata = testdata3)
+p1 <- predict(cart1, newdata = testdata1)
 pred1 <- ifelse(p1[,1] > p1[,2], "Pred: High", "Pred: Low")
 
 cart2 <- rpart(review_scores_rating ~.,data =s2,
                minsplit=30, cp=0.01,
                method = "class")
-p2 <- predict(cart2, newdata = testdata)
-testdata4=testdata[testdata$host_response_time!='None',]
-p2 <- predict(cart2, newdata = testdata4)
-testdata4=testdata4[!(testdata4$neighbourhood_cleansed %in% c('Egger Highlands', 'Lincoln Park', 'Rancho Del Rey')),]
-p2 <- predict(cart2, newdata = testdata4)
+
+p2 <- predict(cart2, newdata = testdata1)
 pred2 <- ifelse(p2[,1] > p2[,2], "Pred: High", "Pred: Low")
 
 
 cart3 <- rpart(review_scores_rating ~.,data =s3,
                minsplit=30, cp=0.01,
                method = "class")
-p3 <- predict(cart3, newdata = testdata)
-testdata5=testdata[testdata$host_response_time!='None',]
-p3 <- predict(cart3, newdata = testdata5)
-testdata5=testdata5[!(testdata5$neighbourhood_cleansed %in% c('Lincoln Park', 'Rancho Del Rey')),]
-p3 <- predict(cart3, newdata = testdata5)
+p3 <- predict(cart3, newdata = testdata1)
 pred3 <- ifelse(p3[,1] > p3[,2], "Pred: High", "Pred: Low")
 
+cart4 <- rpart(review_scores_rating ~.,data =s4,
+               minsplit=30, cp=0.01,
+               method = "class")
+p4 <- predict(cart4, newdata = testdata1)
+pred4 <- ifelse(p4[,1] > p4[,2], "Pred: High", "Pred: Low")
+cart5 <- rpart(review_scores_rating ~.,data =s5,
+               minsplit=30, cp=0.01,
+               method = "class")
+p5 <- predict(cart5, newdata = testdata1)
+pred5 <- ifelse(p5[,1] > p5[,2], "Pred: High", "Pred: Low")
 
-testdata6=testdata4[!(testdata4$neighbourhood_cleansed %in% 
-                        c('Egger Highlands', 'Lincoln Park', 'Rancho Del Rey','Terra Nova')),]
-p1 <- predict(cart1, newdata = testdata6)
-pred1 <- ifelse(p1[,1] > p1[,2], "Pred: High", "Pred: Low")
-p2 <- predict(cart2, newdata = testdata6)
-pred2 <- ifelse(p2[,1] > p2[,2], "Pred: High", "Pred: Low")
-p3 <- predict(cart3, newdata = testdata6)
-pred3 <- ifelse(p3[,1] > p3[,2], "Pred: High", "Pred: Low")
-
-
-preds <- c(pred1, pred2, pred3)
-recs <- as.integer(names(preds)); fin.pred <- rep(0, dim(testdata6)[1])
-for(i in 1:dim(testdata6)[1]){
-  t <- table(preds[which(recs==as.integer(rownames(testdata6))[i])])
+preds <- c(pred1, pred2, pred3,pred4, pred5)
+recs <- as.integer(names(preds)); fin.pred <- rep(0, dim(testdata1)[1])
+for(i in 1:dim(testdata1)[1]){
+  t <- table(preds[which(recs==as.integer(rownames(testdata1))[i])])
   fin.pred[i] <- names(t)[t == max(t)]
 }
-bag.t <- table(fin.pred, testdata6$review_scores_rating) # Contingency table
+bag.t <- table(fin.pred, testdata1$review_scores_rating) # Contingency table
 bag.t
 
 
 
-t=table(preds[which(recs==as.integer(rownames(testdata6))[888])])           
+t=table(preds[which(recs==as.integer(rownames(testdata1))[888])])           
 fin.pred[888]=names(t)[t == max(t)]
-#fin.pred     High  Low
-#  Pred: High 1170  463
-#  Pred: Low   112  152
+# fin.pred     High  Low
+# Pred: High 1165  449
+# Pred: Low   122  168
+(1165+168)/1904 #0.700105
 
-(1170+152)/1897 #0.6968898
+getwd()
